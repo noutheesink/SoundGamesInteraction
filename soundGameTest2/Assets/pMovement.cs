8eb14f049230317b60eb31cec1fda46cc8ab2b39
Cooncube <koenvanderwaal@hotmail.com>
2021-10-19 17:09:58 +0200

    nieuwe scene met Steam Audio plugin

---
+++ soundGameTest2/Assets/pMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pMovement : MonoBehaviour
+{
+    
+    public CharacterController controller;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move);
+    }
+}
